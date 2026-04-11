8eded2235d91c373ab1cf95e1cabc2bf24912a71
starrynightmobiles <starrynight2587@gmail.com>
2026-04-12 00:39:33 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native"
+
+export default function App() {
+  return (
+    <View style={{ marginTop: 50 }}>
+      <Text>Starry Nights App</Text>
+    </View>
+  )
+}
